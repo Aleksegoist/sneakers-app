3de270f0033e39d198cf0fdf5a983868073247aa
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-05 19:46:58 +0300

    add

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 
 const App = () => {
     return (
-        <div className='wrapper'>
-            <header className='d-flex justify-between'>
+        <div className='wrapper clear'>
+            <header className='d-flex justify-between align-center p-20'>
                 <div className='d-flex align-center'>
                     <img width={70} height={70} src={Logo} alt='logo' />
-                    <div className='headerInfo'>
-                        <h3>AY dev. SNEAKERS</h3>
+                    <div className='headerInfo align-center'>
+                        <h3 className='text-uppercase'>AY dev. SNEAKERS</h3>
                         <p>NEW BALANCE</p>
                     </div>
                 </div>
-                <ul className='headerRight'>
-                    <li>
+                <ul className='d-flex'>
+                    <li className='mr-30'>
                         <ShoppingCartOutlinedIcon />
                         <span>$300</span>
                     </li>
