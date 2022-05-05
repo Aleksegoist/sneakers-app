f2a2ec45e53e28e0791d771b5856631698a2f3ef
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-05 18:10:00 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import './index.scss';
 
 const App = () => {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
-}
+    return (
+        <div className='wrapper'>
+            <header>
+                <svg />
+                <div>
+                    <h3>AY dev. SNEAKERS</h3>
+                    <p>NEW BALANCE</p>
+                </div>
+                <ul>
+                    <li>
+                        <svg />
+                        <span>$300</span>
+                    </li>
+                    <li>
+                        <svg />
+                    </li>
+                </ul>
+            </header>
+        </div>
+    );
+};
 
 export default App;
