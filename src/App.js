9a3ab5af7fb4c5e73e4cae5392b93b523556445c
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-07 21:07:52 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,67 +1,10 @@
-import './index.scss';
-import Card from './components/Card';
-import Header from './components/Header';
-import Drawer from './components/Drawer';
-import 'macro-css';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import { useEffect, useState } from 'react';
+
+
 
 const App = () => {
-    const [items, setItems] = useState([]);
-    const [cartItems, setCartItems] = useState([]);
-    const [cartOpened, setCartOpened] = useState(false);
-
-    useEffect(() => {
-        fetch('https://627678f215458100a6af8ae5.mockapi.io/items')
-            .then((res) => {
-                return res.json();
-            })
-            .then((json) => {
-                setItems(json);
-            });
-    }, []);
-
     return (
-        <div className='wrapper clear'>
-            {cartOpened && (
-                <Drawer
-                    onCloseDrawer={() => {
-                        setCartOpened(false);
-                    }}
-                />
-            )}
-            <Header
-                onClickCart={() => {
-                    setCartOpened(true);
-                }}
-            />
-            <div className='content p-20'>
-                <div className='d-flex align-center justify-between'>
-                    <h1>Все кроссовки</h1>
+        <div>Helo World!</div>
 
-                    <div className='search-block d-flex align-center'>
-                        <SearchOutlinedIcon color='disabled' />
-                        <input type='text' placeholder='Поиск...' />
-                    </div>
-                </div>
-
-                <div className='containerCards d-flex flex-wrap justify-between'>
-                    {items.map((obj) => {
-                        return (
-                            <Card
-                                title={obj.title}
-                                price={obj.price}
-                                imageUrl={obj.imageUrl}
-                                onFavorite={() =>
-                                    console.log('Add to favorite')
-                                }
-                                onPlus={() => console.log('Click plus')}
-                            />
-                        );
-                    })}
-                </div>
-            </div>
-        </div>
     );
 };
 
