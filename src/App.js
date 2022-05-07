e8c326cb0f61147ea28f507777640210f8845a15
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-07 22:51:52 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-
-
+import './index.css'
+import {
+    Routes,
+    Route,
+    BrowserRouter,
+} from "react-router-dom";
+import { HomePage } from './pages/home-page';
+import { Header } from './components/header';
 
 const App = () => {
     return (
-        <div>Helo World!</div>
-
+        <BrowserRouter>
+            <div className="App">
+                <Header />
+                <Routes>
+                    <Route path='/' element={<HomePage />} />
+                </Routes>
+            </div>
+        </BrowserRouter >
     );
 };
 
