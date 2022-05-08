4838c38b87651aef339838f4e343dc7d85cfecff
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-05-08 10:19:06 +0300

    add

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,15 +6,7 @@ import Logo from '../../img/logo.png';
 export const Header = () => {
     return (
         <div className='header'>
-            <div className='wrapper'>
-                <div className='logoImg'>
-                    <img src={Logo} alt='logo' className='logo-header' />
-                </div>
-                <Link to='/' className='header__store-title'>
-                    AY SNEAKERS SHOP
-                </Link>
-            </div>
-            <div className='cart__header'>cart</div>
+           
         </div>
     );
 };
